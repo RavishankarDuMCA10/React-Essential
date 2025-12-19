086edba996a00d52dbad0cf40ab011b4c26eaa4d
Ravi Shankar Kushwaha <ravis.du.mca10@gmail.com>
2025-12-19 17:07:45 +0530

    Destructuring arrays

--- react-project/src/main.jsx
+++ react-project/src/main.jsx
@@ -2,4 +2,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const [, , third] = ["giraffe", "elephant", "lion"];
+console.log(third); 
+
+const [first] = ["giraffe", "elephant", "lion"];
+console.log(first); 
+
 createRoot(document.getElementById('root')).render(<App />);
