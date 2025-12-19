9691b7e835ac5761b1b3cc974058e1364e8f9198
Ravi Shankar Kushwaha <ravis.du.mca10@gmail.com>
2025-12-19 18:16:45 +0530

    Show Open status form useState.

--- react-project/src/main.jsx
+++ react-project/src/main.jsx
@@ -2,10 +2,4 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-const [, , third] = ["giraffe", "elephant", "lion"];
-console.log(third); 
-
-const [first] = ["giraffe", "elephant", "lion"];
-console.log(first); 
-
 createRoot(document.getElementById('root')).render(<App />);
