f15c86666f2ee666b2d00e3a005ac25754e8826e
Ravi Shankar Kushwaha <ravis.du.mca10@gmail.com>
2025-12-19 16:51:29 +0530

    Using Fragments.

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -31,14 +31,19 @@ const dishObjects = items.map((dish, i) => ({
 
 function Main({dishes}) {
   return (
-    <main>
-      <img src={chef} height={200} alt="A photo of a smiling chef owner" />
-      <ul>
-        {dishes.map((dish) => (
-          <li key={dish.id} style={{ listStyleType: "none" }}>{dish.title}</li>
-        ))}
-      </ul>
-    </main>
+    <>
+      <div>
+        <h2>Welcome to this beatiful restaurant!</h2>
+      </div>
+      <main>
+        <img src={chef} height={200} alt="A photo of a smiling chef owner" />
+        <ul>
+          {dishes.map((dish) => (
+            <li key={dish.id} style={{ listStyleType: "none" }}>{dish.title}</li>
+          ))}
+        </ul>
+      </main>
+    </>
   )
 }
 
